dfa62f32120d20c80ab8e190030f0b59e3b5a2ae
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-20 15:04:05 +0200

    Initialized Repo

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('ciao')
+
+// CHIEDO IL TRAGITTO IN KM
+const tragittoInChilometri = prompt('Quanti km hai intenzione di percorrere?')
+
+console.log(tragittoInChilometri)
+
+// CHIEDO L'ETA' DEL PASSEGGERO
+const etaPasseggero = prompt('Quanti anni ha il passeggero?')
+
+console.log(etaPasseggero)
