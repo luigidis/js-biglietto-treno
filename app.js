bbb97a755407f1feac13615b5bcfca68927dc6ff
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-20 20:07:47 +0200

    Fixed if control with else

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ if ( !isNaN(itineraryLength) && !isNaN(customerAge)) {
         document.write('Il prezzo del biglietto è: €\n' + ticketFullPrice);
     }   else if (customerAge < 18) {
             document.write('Il prezzo del biglietto è: €\n' + (Number.parseFloat(ticketFullPrice * 0.80).toFixed(2)))
-        }   if (customerAge >65) {
-                document.write('Il prezzo del biglietto è: €\n' + (Number.parseFloat(ticketFullPrice * 0.60).toFixed(2)))
-            }
-                console.log(ticketFullPrice)
+    }   else {
+        document.write('Il prezzo del biglietto è: €\n' + (Number.parseFloat(ticketFullPrice * 0.60).toFixed(2)))
+    }
+        console.log(ticketFullPrice)
 }
 // SE L'UTENTE NON HA NE SOTTO I 18 NE SOPRA I 65 ANNI ALLORA NON APPLICO SCONTO
 
