cb45121f481b1d084cdcb13fbc6c227be40c49e9
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-20 16:12:22 +0200

    Esercizio Completo

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 console.log('ciao')
 
 // CHIEDO IL TRAGITTO IN KM
-const tragittoInChilometri = prompt('Quanti km hai intenzione di percorrere?')
+const itineraryLength = parseFloat( prompt('Quanti km hai intenzione di percorrere?'))
 
-console.log(tragittoInChilometri)
+console.log(itineraryLength)
 
 // CHIEDO L'ETA' DEL PASSEGGERO
-const etaPasseggero = prompt('Quanti anni ha il passeggero?')
+const customerAge = parseInt( prompt('Quanti anni ha il passeggero?'))
 
-console.log(etaPasseggero)
+console.log(customerAge)
+
+// MI CALCOLO IL PREZZO PIENO
+
+const ticketFullPrice = (Number.parseFloat(0.21 * itineraryLength).toFixed(2))
+console.log (ticketFullPrice)
+
+if ( !isNaN(itineraryLength) && !isNaN(customerAge)) {
+    if ( (customerAge >= 18) && (customerAge <= 65) ) {
+        document.write('Il prezzo del biglietto è: €\n' + ticketFullPrice);
+    }   else if (customerAge < 18) {
+            document.write('Il prezzo del biglietto è: €\n' + (Number.parseFloat(ticketFullPrice * 0.80).toFixed(2)))
+        }   if (customerAge >65) {
+                document.write('Il prezzo del biglietto è: €\n' + (Number.parseFloat(ticketFullPrice * 0.60).toFixed(2)))
+            }
+                console.log(ticketFullPrice)
+}
+// SE L'UTENTE NON HA NE SOTTO I 18 NE SOPRA I 65 ANNI ALLORA NON APPLICO SCONTO
+
+// SE L'UTENTE HA MENO DI 18 ANNI APPLICO UNO SCONTO DEL 20%
+
+// SE L'UTENTE HA PIU' DI 65 ANNI APPLICO UNO SCONTO DEL 40%
+
+
+
+
+
+
+
+
